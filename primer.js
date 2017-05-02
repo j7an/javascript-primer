c7ae806aa47f4076275032decba1823ba6582818
Jia Tan <j7an@users.noreply.github.com>
2017-05-01 21:29:28 -0700

    Update

--- primer.js
+++ primer.js
@@ -1,3 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var products = [
     { name: "Hat", price: 24.5, stock: 10 },
     { name: "Kayak", price: 289.99, stock: 1 },
@@ -8,4 +10,15 @@ var totalValue = products
     .filter(function (item) { return item.stock > 0; })
     .reduce(function (prev, item) { return prev + (item.price * item.stock); }, 0);
 console.log("Total value: $" + totalValue.toFixed(2));
+var NameAndWeather_1 = require("./modules/NameAndWeather");
+var DuplicateName_1 = require("./modules/DuplicateName");
+var tempConverter_1 = require("./tempConverter");
+var name = new NameAndWeather_1.Name("Adam", "Freeman");
+var loc = new NameAndWeather_1.WeatherLocation("raining", "London");
+var other = new DuplicateName_1.Name();
+var cTemp = tempConverter_1.TempConverter.convertFtoC(38);
+console.log(name.nameMessage);
+console.log(loc.weatherMessage);
+console.log(other.message);
+console.log("The temp is " + cTemp + "C");
 //# sourceMappingURL=primer.js.map
--- primer.ts
+++ primer.ts
@@ -10,3 +10,18 @@ let totalValue = products
     .reduce((prev, item) => prev + (item.price * item.stock), 0);
 
 console.log("Total value: $" + totalValue.toFixed(2)); 
+
+import { Name, WeatherLocation } from "./modules/NameAndWeather";
+import { Name as OtherName } from "./modules/DuplicateName";
+import { TempConverter } from "./tempConverter";
+
+let name = new Name("Adam", "Freeman");
+let loc = new WeatherLocation("raining", "London");
+let other = new OtherName();
+
+let cTemp = TempConverter.convertFtoC(38);
+
+console.log(name.nameMessage);
+console.log(loc.weatherMessage);
+console.log(other.message);
+console.log(`The temp is ${cTemp}C`)
